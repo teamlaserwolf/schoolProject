823f62537d990e21320f53968880124c24c23e42
dee <darioush.saffaripoor@gmail.com>
2015-09-26 18:12:21 -0400

    now we can extract data from the database

---
+++ SchoolProject/Services/tester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class tester
+    {
+        public static void Main()
+        {
+            Console.WriteLine("We are live!");
+            tester test = new tester();
+            test.selectEmployee();
+            Console.ReadLine();
+        }
+
+        private void selectEmployee()
+        {
+            EmployeeService employeeService = new EmployeeService();
+            employeeService.selectEmployee(1);
+            employeeService.display();
+        }
+    }
+}
